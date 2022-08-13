a0e01ca951be41dc9d029e810a47f32d0d1b0f15
Mahesh Ravi <maheshm444@gmail.com>
2022-08-13 13:03:57 +0530

    Added email functionality for procedures and medicines

--- index.js
+++ index.js
@@ -12,6 +12,12 @@ const transporter = nodemailer.createTransport({
     pass: process.env.PASSWORD,
   },
 })
+
+app.get('/', async (req, res) => {
+
+  res.send({code: 200, "message":"Connected successfully"})
+})
+
 app.post('/', async (req, res) => {
   const user = req.body.user ? req.body.user : 'User'
   const toEmail = req.body.userEmail ? req.body.userEmail : ''
@@ -26,8 +32,6 @@ app.post('/', async (req, res) => {
   req.body.links &&
     req.body.links.length > 0 &&
     req.body.links.map((link) => mediaLinks.push(link))
-
-  console.log('mediaLinks', mediaLinks)
 
   await transporter
     .sendMail({
@@ -54,4 +58,83 @@ app.post('/', async (req, res) => {
   res.json({ status: 200, message: 'Email sent successfully' })
 })
 
-app.listen('5000', () => console.log('listening on port 5000'))
+app.post('/order-medicine', async (req, res) => {
+
+const {address, user, userEmail, type, medicines, links } = req.body
+
+  const mediaLinks = []
+  links &&
+    links.length > 0 &&
+    links.map((link) => mediaLinks.push(link))
+
+  console.log('mediaLinks', mediaLinks)
+
+  await transporter
+    .sendMail({
+      from: 'dralkas.backend@gmail.com',
+      to: userEmail,
+      subject: `Email from Dr. Alkas Team - ${type}`,
+      text: `Dear ${user != '' ? user : 'User'},
+
+      Greetings from Dr.Alkas Team, Your ${type} has been booked with our Clinic.
+
+      Booking Details:
+      Medicines Booked: 
+      ${medicines.map(medicine => 
+      `
+        Medicine Name: ${medicine.name}
+        Cost: ${medicine.price}
+        Quantity: ${medicine.quantity}
+        Expiry: ${(medicine.expiry!= '' && medicine.expiry!= undefined ) ? medicine.expiry : "Not Applicable"}
+        `
+        )
+      }
+      Shipto Address: ${address}
+      Prescriptions Submitted by the patient: ${mediaLinks.map(
+        (media) => media + ' '
+      )}
+      
+      Regards
+      Dr.Alkas Team`,
+    })
+    .then(console.log('Email sent'))
+    .catch((e) => res.json({ status: 400, message: 'Error ocurred' }))
+  res.json({ status: 200, message: 'Medicine orders email sent successfully' })
+})
+
+app.post('/order-products', async (req, res) => {
+
+  const {address, user, userEmail, type, products } = req.body
+  
+    await transporter
+      .sendMail({
+        from: 'dralkas.backend@gmail.com',
+        to: userEmail,
+        subject: `Email from Dr. Alkas Team - ${type}`,
+        text: `Dear ${user != '' ? user : 'User'},
+  
+        Greetings from Dr.Alkas Team, Your ${type} has been booked with our Clinic.
+  
+        Booking Details:
+        Products Booked: 
+        ${products.map(product => 
+        `
+          Product Name: ${product.name}
+          Cost: ${product.price}
+          Quantity: ${product.quantity}
+          Expiry: ${(product.expiry!= '' && product.expiry!= undefined ) ? product.expiry : "Not Applicable"}
+          `
+          )
+        }
+        Shipto Address: ${address}
+        
+        Regards
+        Dr.Alkas Team`,
+      })
+      .then(console.log('Email sent'))
+      .catch((e) => res.json({ status: 400, message: 'Error ocurred' }))
+    res.json({ status: 200, message: 'Product orders email sent successfully' })
+  })
+
+
+app.listen('3000', () => console.log('listening on port 3000'))
